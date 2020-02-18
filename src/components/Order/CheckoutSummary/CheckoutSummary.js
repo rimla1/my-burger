ddcbf9d59d7ec1b8cb2f99491dfb64c419154557
Almir Muminovic <projectalmir@gmail.com>
2020-02-18 14:22:25 +0100

    Checkout: Back-Next page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,18 +4,18 @@ import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = (props) => {
-    return(
+    return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Burger ingredients={props.ingredients}/>
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Burger ingredients={props.ingredients} />
             </div>
             <Button btnType='Danger'
-            clicked
+                clicked={props.checkoutCancelled}
             >Cancel</Button>
-            
+
             <Button btnType='Success'
-            clicked
+                clicked={props.checkoutContinued}
             >Continue</Button>
         </div>
     )
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
-   state ={
-       ingredients: {
-           salad: 1,
-           meat: 1,
-           cheese: 1,
-           bacon: 1
-       }
-   }
-   
-    render () {
-        return(
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
+
+
+    checkoutCancelledHandler = (props) => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = (props) => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
+    render() {
+        return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}
+                />
             </div>
         )
     }
