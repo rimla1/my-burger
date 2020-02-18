9366dd4405da7bcc937610c97f11e0ac3e028ab9
Almir Muminovic <projectalmir@gmail.com>
2020-02-18 14:07:50 +0100

    Navigating to Checkout Page

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,19 +1,22 @@
 import React from 'react';
+
+import { withRouter } from 'react-router-dom';
+
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-          return  <BurgerIngredient key={igKey + i } type={igKey} />
-        });
-    })
-    .reduce((arr, el) => {
-        return arr.concat(el)
-    }, []);
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
 
-    if(transformedIngredients.length === 0) {
+    if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients!</p>
     }
 
@@ -27,4 +30,4 @@ const burger = (props) => {
     );
 };
 
-export default burger;
+export default withRouter(burger);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -86,30 +86,31 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true })
         // alert('you continue!');
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Almir Muminovic',
-                address: {
-                    street: 'Svojbor',
-                    zipCode: '80',
-                    country: 'Serbia'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Almir Muminovic',
+        //         address: {
+        //             street: 'Svojbor',
+        //             zipCode: '80',
+        //             country: 'Serbia'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+        this.props.history.push('/checkout')
     }
 
 
@@ -142,8 +143,9 @@ class BurgerBuilder extends Component {
             orderSummary = <OrderSummary
                 ingredients={this.state.ingredients}
                 purchaseCancelled={this.purchaseCancelHandler}
+                price={this.state.totalPrice}
                 purchaseContinued={this.purchaseContinueHandler}
-                price={this.state.totalPrice} />
+            />
         }
 
         if (this.state.loading) {
