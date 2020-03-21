6fc1f290f77264f2bb627252b169d52c74820d80
Almir Muminovic <projectalmir@gmail.com>
2020-03-21 22:45:10 +0100

    Adding an orders page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -45,6 +45,7 @@ class Checkout extends Component {
                 <Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
 
             </div>
+
         )
     }
 
